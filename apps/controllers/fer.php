8f038e53065255313c5be12915733a8af93e647a
Fabrice Dant <fabrice.dant@yesnyou.com>
2016-03-04 15:42:04 +0100

    Premières versions de test pour lsat

--- apps/controllers/fer.php
+++ apps/controllers/fer.php
@@ -8,13 +8,11 @@ use FragTale\Controller;
 class Fer extends Controller{
 	
 	protected static $doceboInstances = array(
-		//Sandbox
+		//Sandbox AFOSCHI
 		'default'	=> array(
-			'docebo_api_1'	=> array(
-					'url'		=> 'http://afoschi-etime-git.docebo.info/api/yny_learningplan/getEvaluationData',
-					'api_key'	=> 't_!XDUubB_PlQdqvs55OQGwL',
-					'api_secret'=> 'rgPBl*Lhv0tX4sqk-n2xCxw*r_CDnJU0CY-V'
-			),
+			'url'		=> 'http://afoschi-etime-git.docebo.info/api/',
+			'api_key'	=> 't_!XDUubB_PlQdqvs55OQGwL',
+			'api_secret'=> 'rgPBl*Lhv0tX4sqk-n2xCxw*r_CDnJU0CY-V'
 		),
 	);
 	
@@ -28,50 +26,99 @@ class Fer extends Controller{
 	}
 	
 	function main(){
-		$this->_view->json = $this->retrieve();
+		$this->_view->json = array(
+			'User'		=>$this->retrieveUserData(),
+			'LP_Data'	=>$this->retrieveLpDataForUser()
+		);
 	}
 	
 	/**
-	 * Get JSON result from Docebo API, giving method name (example: user/listUsers)
-	 * and an array of parameters (example: array('id_user'=>56456)).
-	 * @return JSON string
+	 * @see https://doceboapi.docebosaas.com/api/docs
+	 * @param string $method	The original method name given by the Docebo API such as "user/profile"
+	 * @param array $postParams
+	 * @return array (for JSON encode)
 	 */
-	function retrieve(){
+	function retrieve($method='', $postParams=array()){
+		if (empty($method)){
+			if (!empty($_REQUEST['method']))
+				$method = $_REQUEST['method'];
+			else
+				return $this->returnJsonError('Missing required "method" parameter');
+		}
+		//Find key, url and secret
+		$clientHost = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+		if (isset(self::$doceboInstances[$clientHost])){
+			if (empty(self::$doceboInstances[$clientHost]['url']))
+				return $this->returnJsonError('Development error: URL property not defined for "'.$clientHost.'" instance.');
+			else
+				$curlParams = self::$doceboInstances[$clientHost];
+		}
+		else{
+			if (empty(self::$doceboInstances['default']['url']))
+				return $this->returnJsonError('Development error: URL property not defined for "default" instance.');
+			else
+				$curlParams = self::$doceboInstances['default'];
+		}
+	
+		$url	= trim($curlParams['url'], '/').'/'.trim($method, '/');
+		$sha1	= sha1(implode(",", $postParams) . "," . $curlParams['api_secret']);
+		$code	= base64_encode($curlParams['api_key'] . ":" . $sha1);
+		$result = null;
+	
+		try{
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_POST, count($postParams));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postParams));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Authorization: Docebo " . $code));
+		
+			$result = curl_exec($ch);
+		}
+		catch(\Exception $exc){
+			$this->returnJsonError('Exception code '.$exc->getCode().': '.$exc->getMessage());
+		}
+		
+		if (false === $result)
+			return $this->returnJsonError('Curl error #' . curl_errno($ch) . ': ' . curl_error($ch));
+		else
+			return json_decode($result, true);
+	}
+	
+	/**
+	 * @param string $message
+	 * @return array
+	 */
+	function returnJsonError($message){
+		return array(
+			'success'	=> 0,
+			'message'	=> $message
+		);
+	}
+	
+	function retrieveUserData(){
+		if (empty($_REQUEST['id_user'])){
+			header('HTTP/1.0 201 Mandatory input parameter is missing');
+			exit;
+		}
+		$postParams = array(
+			'id_user'	=> $_REQUEST['id_user'],
+		);
+		return $this->retrieve('user/profile', $postParams);
+	}
+	
+	function retrieveLpDataForUser(){
 		//Check arguments
-		if (empty($_REQUEST['id_user']) || empty($_REQUEST['id_learningplan']))
-			return false;
+		if (empty($_REQUEST['id_user']) || empty($_REQUEST['id_learningplan'])){
+			header('HTTP/1.0 201 Mandatory input parameter is missing');
+			exit;
+		}
 		$postParams = array(
 			'id_user'         => $_REQUEST['id_user'],
 			'id_learningplan' => $_REQUEST['id_learningplan']
 		);
-		//Find key, url and secret
-		$clientHost = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-		if (isset(self::$doceboInstances[$clientHost]))
-			$curlParams = self::$doceboInstances[$clientHost]['docebo_api_1'];
-		else
-			$curlParams = self::$doceboInstances['default']['docebo_api_1'];
-	
-		$sha1 = sha1(implode(",", $postParams) . "," . $curlParams['api_secret']);
-		$code = base64_encode($curlParams['api_key'] . ":" . $sha1);
-	
-		$ch = curl_init($curlParams['url']);
-		curl_setopt($ch, CURLOPT_POST, count($postParams));
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postParams));
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Authorization: Docebo " . $code));
-	
-		$result = curl_exec($ch);
-	
-		if (false === $result) {
-			return array(
-				'success' => '0',
-				'message' => 'Curl error #' . curl_errno($ch) . ': ' . curl_error($ch)
-			);
-		}
-		else
-			return json_decode($result, true);
+		return $this->retrieve('yny_learningplan/getEvaluationData', $postParams);
 	}
 	
 	/**
--- apps/controllers/fer/create.php
+++ apps/controllers/fer/create.php
@@ -26,32 +26,94 @@ class Create extends Fer{
 	
 	protected $postDone = false;
 	
+	function initialize(){
+		if (!empty($_REQUEST['layout']))
+			$this->setLayout(strtolower($_REQUEST['layout']));
+		else
+			$this->setLayout('json');
+		$this->_view->json = array();
+	}
+	
 	/**
 	 * As for Curl, the call to this page must be done in POST method (submission)
 	 * {@inheritDoc}
 	 * @see \FragTale\Controller\Fer::doPostBack()
 	 */
 	function doPostBack(){
-		foreach ($this->expectedParameters as $pK=>$isRequired){
-			if ($isRequired && empty($_POST[$pK])){
-				header('HTTP/1.0 201 Mandatory input parameter is missing');
-				exit;
-			}
-			$this->expectedParameters[$pK] = !empty($_POST[$pK]) ? $_POST[$pK] : null;
-		}
-		if (!in_array(strtolower($this->expectedParameters['report_mode']), array('r', 'w'))){
-			header('HTTP/1.0 204 Invalid report_mode');
-			exit;
-		}
-		
-		$this->postDone = true;
+		//return $this->processing();
 	}
 	
 	function main(){
-		if (!$this->postDone){
-			header('HTTP/1.0 403 Forbidden');
-			die('Forbidden');
+		if ($this->processing()){
+			if (!$this->postDone){
+				header('HTTP/1.0 403 Forbidden');
+				die('Forbidden');
+			}
 		}
+	}
+	
+	function processing(){
+		foreach ($this->expectedParameters as $pK=>$isRequired){
+			if ($isRequired && empty($_REQUEST[$pK])){
+				header('HTTP/1.0 201 Mandatory input parameter is missing');
+				die('Mandatory input parameter is missing');
+			}
+			$this->expectedParameters[$pK] = !empty($_REQUEST[$pK]) ? $_REQUEST[$pK] : null;
+		}
+		if (!in_array(strtolower($this->expectedParameters['report_mode']), array('r', 'w'))){
+			header('HTTP/1.0 204 Invalid report_mode');
+			die('Invalid report_mode');
+		}
+		//Get user info
+		$User = $this->retrieveUserData();
+		if (empty($User['success'])){
+			$this->_view->json = $this->returnJsonError('Unknown User');
+			return false;
+		}
+		//Get learning plan
+		$LP	= $this->retrieveLpDataForUser();
+		if (empty($LP['hashcode'])){
+			$this->_view->json = $LP;
+			$this->_view->json['message'] = 'Unabled to retrieve LP Data for given id_user and id_learningplan';
+			return false;
+		}
+		//On creation, we get the current template version
+		$tplId		= strtolower(trim($this->expectedParameters['template_id']));
+		$tplVersion = $this->expectedParameters['template_version'];
+		try{
+			if (!empty($tplVersion)){
+				//Match first if the template version exists
+				if (!file_exists(APP_ROOT.'/templates/views/fer/tpl/versions/'.$tplId.'/'.$tplVersion.'.phtml')){
+					header('HTTP/1.0 203 Invalid combination template id / template version');
+					die('Invalid combination template id / template version');
+				}
+				else{
+					$data = $this->getBlock('fer/tpl/versions/'.$tplId.'/'.$tplVersion, array('User'=>$User, 'LP'=>$LP));
+					$this->jsonResponse['report'] = base64_encode($data);
+				}
+			}
+			else{
+				if (!file_exists(APP_ROOT.'/templates/views/fer/tpl/current/'.$tplId.'.phtml')){
+					header('HTTP/1.0 203 Invalid combination template id / template version');
+					die('Invalid combination template id / template version');
+				}
+				else{
+					$data = $this->getBlock('fer/tpl/current/'.$tplId, array('User'=>$User, 'LP'=>$LP));
+					$this->jsonResponse['report'] = base64_encode($data);
+				}
+			}
+		}
+		catch(\Exception $exc){
+			print_r($exc);
+			print_r($this->expectedParameters);
+		}
+		$this->jsonResponse['template_id']		= $tplId;
+		$this->jsonResponse['template_version'] = str_replace($tplId.'/', '', $this->getMetaView()->_tpl_version);
+		$this->jsonResponse['hashcode']			= $LP['hashcode'];
+		$this->jsonResponse['success']			= 1;
+		$this->_view->json = $this->jsonResponse;
+		$this->postDone = true;
+		return true;
 	}
 	
 }
