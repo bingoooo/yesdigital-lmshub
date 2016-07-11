dfe185111875d5a4693e963f52ebe2bf581a46ee
Fabrice Dant <fabrice.dant@yesnyou.com>
2016-07-11 16:22:49 +0200

    KN date format

--- apps/controllers/yny_reports/export/xlsx/kn.php
+++ apps/controllers/yny_reports/export/xlsx/kn.php
@@ -20,9 +20,9 @@ class Kn extends Xlsx{
 				if (empty($User['learning_plans'])) continue;
 				foreach ($User['learning_plans'] as $path_id=>$LP){
 					$line++;
-					$lpstartdate= (stripos($LP['user_lp_date_begin_validity'], '0000-00-00')!==false || empty($LP['user_lp_date_begin_validity'])) ? '' : date('d/m/Y', strtotime($LP['user_lp_date_begin_validity'])); 
-					$lpenddate	= (stripos($LP['user_lp_date_end_validity'], '0000-00-00')!==false || empty($LP['user_lp_date_end_validity'])) ? '' : date('d/m/Y', strtotime($LP['user_lp_date_end_validity'])); 
-					$this->PHPXL->setActiveSheetIndex(0)
+					$lpstartdate= (stripos($LP['user_lp_date_begin_validity'], '0000-00-00')!==false || empty($LP['user_lp_date_begin_validity'])) ? null : \PHPExcel_Shared_Date::PHPToExcel(strtotime($LP['user_lp_date_begin_validity'])); 
+					$lpenddate	= (stripos($LP['user_lp_date_end_validity'], '0000-00-00')!==false || empty($LP['user_lp_date_end_validity'])) ? null : \PHPExcel_Shared_Date::PHPToExcel(strtotime($LP['user_lp_date_end_validity'])); 
+					$this->XlActiveSheet
 						->setCellValue('A'.$line, strtoupper($User['firstname']))
 						->setCellValue('B'.$line, !empty($User['lastname']) ? strtoupper($User['lastname']) : trim($User['login'], '/'))
 						->setCellValue('C'.$line, $User['email'])
@@ -37,97 +37,67 @@ class Kn extends Xlsx{
 						->setCellValue('N'.$line, '')//On track ??
 					;
 					
-					$elearnings = $microlearnings = $sessions = $lsat = array();
+					$elearnings = $microlearnings = $sessions = array();
 					$globalTime = $iCol = $nbElearnings = $nbElCompleted = $nbSessionPassed = 0;
 					
 					$lastAccess = null;
 					$courseColumns = array('O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
 					//Get courses
-					if (stripos($LP['path_name'], 'lsat')!==false){
-						if (!empty($LP['courses'])){
-							foreach ($LP['courses'] as $course_id=>$Course){
-								$globalTime += (int)$Course['user_course_timespent'];
-								if (!empty($Course['user_course_date_last_access'])){
-									if (!$lastAccess || $lastAccess < $Course['user_course_date_last_access'])
-										$lastAccess = $Course['user_course_date_last_access'];
-								}
-								$Course['module_status'] = (!empty($Course['user_course_date_completed']) && stripos($Course['user_course_date_completed'], '0000-00-00')===false) ? 'Completed' :
-									(empty($Course['user_course_date_first_access']) || stripos($Course['user_course_date_first_access'], '0000-00-00')!==false ? 'Not started' : 'In progress');
-								if ($Course['module_status']==='Completed')
-									$nbElCompleted++;
-								$lsat[$course_id] = $Course;
+					if (!empty($LP['courses'])){
+						foreach ($LP['courses'] as $course_id=>$Course){
+							$globalTime += (int)$Course['user_course_timespent'];
+							if (!empty($Course['user_course_date_last_access'])){
+								if (!$lastAccess || $lastAccess < $Course['user_course_date_last_access'])
+									$lastAccess = $Course['user_course_date_last_access'];
 							}
-						}
-					}
-					else{
-						if (!empty($LP['courses'])){
-							foreach ($LP['courses'] as $course_id=>$Course){
-								$globalTime += (int)$Course['user_course_timespent'];
-								if (!empty($Course['user_course_date_last_access'])){
-									if (!$lastAccess || $lastAccess < $Course['user_course_date_last_access'])
-										$lastAccess = $Course['user_course_date_last_access'];
-								}
-								if ($Course['course_type']==='elearning'){
-									if (stripos($Course['course_name'], 'micro')!==false)
-										$microlearnings[$course_id] = $Course;
-									else{
-										$Course['module_status'] = (!empty($Course['user_course_date_completed']) && stripos($Course['user_course_date_completed'], '0000-00-00')===false) ? 'Completed' :
-											(empty($Course['user_course_date_first_access']) || stripos($Course['user_course_date_first_access'], '0000-00-00')!==false ? 'Not started' : 'In progress');
-										if ($Course['module_status']==='Completed')
-											$nbElCompleted++;
-										$elearnings[$course_id] = $Course;
-									}
-								}
+							if ($Course['course_type']==='elearning'){
+								if (stripos($Course['course_name'], 'micro')!==false)
+									$microlearnings[$course_id] = $Course;
 								else{
-									if ((!empty($Course['user_course_date_completed']) && stripos($Course['user_course_date_completed'], '0000-00-00')===false) || $Course['user_course_status']==2)
-										$nbSessionPassed++;
-									$sessions[$course_id] = $Course;
+									$Course['module_status'] = (!empty($Course['user_course_date_completed']) && stripos($Course['user_course_date_completed'], '0000-00-00')===false) ? 'Completed' :
+										(empty($Course['user_course_date_first_access']) || stripos($Course['user_course_date_first_access'], '0000-00-00')!==false ? 'Not started' : 'In progress');
+									if ($Course['module_status']==='Completed')
+										$nbElCompleted++;
+									$elearnings[$course_id] = $Course;
 								}
+							}
+							else{
+								if ((!empty($Course['user_course_date_completed']) && stripos($Course['user_course_date_completed'], '0000-00-00')===false) || $Course['user_course_status']==2)
+									$nbSessionPassed++;
+								$sessions[$course_id] = $Course;
 							}
 						}
 					}
-					if (!empty($lsat)){
-						$nbElearnings = count($lsat);
-						foreach ($lsat as $course_id=>$Course){
-							$completiondate = (stripos($Course['user_course_date_completed'], '0000-00-00')!==false || empty($Course['user_course_date_completed'])) ? '' : date('d/m/Y', strtotime($Course['user_course_date_completed'])); 
-							$this->PHPXL->setActiveSheetIndex(0)
+					
+					if (!empty($elearnings)){
+						$nbElearnings = count($elearnings);
+						foreach ($elearnings as $course_id=>$Course){
+							$completiondate = (stripos($Course['user_course_date_completed'], '0000-00-00')!==false || empty($Course['user_course_date_completed'])) ? null : \PHPExcel_Shared_Date::PHPToExcel(strtotime($Course['user_course_date_completed'])); 
+							$this->XlActiveSheet
 								->setCellValue($courseColumns[$iCol].$line, $Course['module_status'])
 								->setCellValue($courseColumns[$iCol+1].$line, $completiondate)
 							;
 							$iCol+=2;
 						}
 					}
-					else{
-						if (!empty($elearnings)){
-							$nbElearnings = count($elearnings);
-							foreach ($elearnings as $course_id=>$Course){
-								$completiondate = (stripos($Course['user_course_date_completed'], '0000-00-00')!==false || empty($Course['user_course_date_completed'])) ? '' : date('d/m/Y', strtotime($Course['user_course_date_completed'])); 
-								$this->PHPXL->setActiveSheetIndex(0)
-									->setCellValue($courseColumns[$iCol].$line, $Course['module_status'])
-									->setCellValue($courseColumns[$iCol+1].$line, $completiondate)
-								;
-								$iCol+=2;
-							}
-						}
-						if (!empty($sessions)){
-							$this->PHPXL->setActiveSheetIndex(0)
-								->setCellValue('AC'.$line, count($sessions))
-								->setCellValue('AD'.$line, $nbSessionPassed)
-							;
-						}
-						if (!empty($microlearnings)){
-							$Course = reset($microlearnings);
-							$mlStatus = (!empty($Course['user_course_date_completed']) && stripos($Course['user_course_date_completed'], '0000-00-00')===false) ? 'Completed' :
-											(empty($Course['user_course_date_first_access']) || stripos($Course['user_course_date_first_access'], '0000-00-00')!==false ? 'Not started' : 'In progress');
-							$this->PHPXL->setActiveSheetIndex(0)
-								->setCellValue('AA'.$line, $mlStatus)
-								->setCellValue('AM'.$line, '')//Completion of ML
-							;
-						}
+					if (!empty($sessions)){
+						$this->XlActiveSheet
+							->setCellValue('AC'.$line, count($sessions))
+							->setCellValue('AD'.$line, $nbSessionPassed)
+						;
+					}
+					if (!empty($microlearnings)){
+						$Course = reset($microlearnings);
+						$mlStatus = (!empty($Course['user_course_date_completed']) && stripos($Course['user_course_date_completed'], '0000-00-00')===false) ? 'Completed' :
+										(empty($Course['user_course_date_first_access']) || stripos($Course['user_course_date_first_access'], '0000-00-00')!==false ? 'Not started' : 'In progress');
+						$this->XlActiveSheet
+							->setCellValue('AA'.$line, $mlStatus)
+							->setCellValue('AM'.$line, '')//Completion of ML
+						;
 					}
 					//$completion
-					$lastAccess = (stripos($lastAccess, '0000-00-00')!==false || empty($lastAccess)) ? '' : date('d/m/Y', strtotime($lastAccess)); 
-					$this->PHPXL->setActiveSheetIndex(0)
+					$lastAccess = (stripos($lastAccess, '0000-00-00')!==false || empty($lastAccess)) ? null : \PHPExcel_Shared_Date::PHPToExcel(strtotime($lastAccess)); 
+					$this->XlActiveSheet
 						->setCellValue('L'.$line, $nbElearnings)//Amount of modules in learning plan
 						->setCellValue('AE'.$line, $globalTime)//Total time en heures
 						->setCellValue('AF'.$line, $lastAccess)
@@ -145,6 +115,12 @@ class Kn extends Xlsx{
 			$this->XlActiveSheet->getStyle($aCol.'2:'.$aCol.$finalrowindex)
 			->getAlignment()
 			->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+		}
+		//Set date format
+		foreach (array('J', 'K', 'P', 'R', 'T', 'V', 'X', 'Z', 'AF') as $aCol){
+			$this->XlActiveSheet->getStyle($aCol.'2:'.$aCol.$finalrowindex)
+			->getNumberFormat()
+			->setFormatCode(\PHPExcel_Style_NumberFormat::FORMAT_DATE_DDMMYYYY);
 		}
 	}
 	
