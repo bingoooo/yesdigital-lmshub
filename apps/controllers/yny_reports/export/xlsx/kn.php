dcbdc5a8947a84abecfc04bde8229094eb283ae0
Fabrice Dant <fabrice.dant@yesnyou.com>
2016-09-08 14:58:07 +0200

    YNY_REPORTS:KN: 2 new columns (ESP + catchups). Fixe date & time format

--- apps/controllers/yny_reports/export/xlsx/kn.php
+++ apps/controllers/yny_reports/export/xlsx/kn.php
@@ -36,8 +36,8 @@ class Kn extends Xlsx{
 						->setCellValue('N'.$line, '')//On track ??
 					;
 					
-					$elearnings = $microlearnings = $sessions = array();
-					$globalTime = $iCol = $nbElearnings = $nbElCompleted = $nbSessionPassed = 0;
+					$elearnings = $microlearnings = $sessions = $esps = $catchups = array();
+					$globalTime = $iCol = $nbElearnings = $nbElCompleted = $nbSessionPassed = $nbCatchupsTaken = 0;
 					
 					$lastAccess = null;
 					$courseColumns = array('O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
@@ -67,6 +67,32 @@ class Kn extends Xlsx{
 							}
 						}
 					}
+					// handle catchups & ESPs as they have no learning plans.
+					// + They need to be displayed only once in the first LP found
+					if (!empty($User['learning_plans']['UNKNOWN']['courses'])){
+						foreach ($User['learning_plans']['UNKNOWN']['courses'] as $course_id=>$Course){
+							if (!empty($Course['user_course_date_last_access'])){
+								if (!$lastAccess || $lastAccess < $Course['user_course_date_last_access'])
+									$lastAccess = $Course['user_course_date_last_access'];
+							}
+							if(stripos($Course['course_code'], 'ESP')!==false){
+								if (empty($User['learning_plans']['UNKNOWN']['esp_counted_yet']))
+									$esps[$course_id] = $Course;
+								continue;
+							}
+							$catchpos = stripos($Course['course_code'], 'CATCH');
+							if (!empty($sessions) && $catchpos!==false){
+								// We have to match if this catchup match a corresponding SKS in this Learning plan
+								$exp2match = trim(substr($Course['course_code'], 0, $catchpos), '_');
+								foreach ($sessions as $session){
+									if (stripos($session['course_code'], $exp2match)!==false){
+										$catchups[$course_id] = $Course;
+									}
+								}
+							}
+						}
+						$User['learning_plans']['UNKNOWN']['esp_counted_yet'] = true;
+					}
 					
 					if (!empty($elearnings)){
 						$nbElearnings = count($elearnings);
@@ -79,6 +105,8 @@ class Kn extends Xlsx{
 							$iCol+=2;
 						}
 					}
+					$this->XlActiveSheet->setCellValue('L'.$line, $nbElearnings);//Amount of modules in learning plan
+					
 					if (!empty($sessions)){
 						$this->XlActiveSheet
 							->setCellValue('AC'.$line, count($sessions))
@@ -94,13 +122,28 @@ class Kn extends Xlsx{
 							->setCellValue('AM'.$line, '')//Completion of ML
 						;
 					}
+					if (!empty($esps)){
+						$esp_time = $esp_done = 0;
+						foreach ($esps as $course_id => $Course){
+							$esp_time += $Course['user_course_timespent'];
+							if($Course['user_course_status'] == 2) $esp_done++;
+						}
+						$this->XlActiveSheet->setCellValue('AF'.$line, $esp_done.'/'.count($esps));
+						$globalTime += $esp_time;
+					}
+					if (!empty($catchups)){
+						$cu_time = 0;
+						$cu_count= count($catchups);
+						foreach ($catchups as $course_id => $Course){
+							$cu_time += $Course['user_course_timespent'];
+							if($Course['user_course_status'] == 2) $nbCatchupsTaken++;
+						}
+						$this->XlActiveSheet->setCellValue('AE'.$line, $nbCatchupsTaken);
+						$globalTime += $cu_time;
+					}
 					//$completion
-					$lastAccess = (stripos($lastAccess, '0000-00-00')!==false || empty($lastAccess)) ? null : \PHPExcel_Shared_Date::PHPToExcel(strtotime($lastAccess)); 
-					$this->XlActiveSheet
-						->setCellValue('L'.$line, $nbElearnings)//Amount of modules in learning plan
-						->setCellValue('AE'.$line, $globalTime)//Total time en heures
-						->setCellValue('AF'.$line, $lastAccess)
-					;
+					$this->XlActiveSheet->setCellValue('AG'.$line, ($globalTime/86400), \PHPExcel_Cell_DataType::TYPE_NUMERIC);//Total time en heures
+					$this->XlActiveSheet->setCellValue('AH'.$line, $this->toExcelDateFormat($lastAccess));
 				}
 			}
 			$this->setExcelFinalFormat($line);
@@ -110,16 +153,23 @@ class Kn extends Xlsx{
 	
 	function setExcelFinalFormat($finalrowindex){
 		//Set center alignment for columns
-		foreach (array('G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'T', 'V', 'X', 'Z', 'AB', 'AC', 'AD', 'AE', 'AF') AS $aCol){
+		foreach (array('G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'T', 'V', 'X', 'Z', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH') AS $aCol){
 			$this->XlActiveSheet->getStyle($aCol.'2:'.$aCol.$finalrowindex)
 			->getAlignment()
 			->setHorizontal(\PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 		}
 		//Set date format
-		foreach (array('J', 'K', 'P', 'R', 'T', 'V', 'X', 'Z', 'AF') as $aCol){
+		foreach (array('J', 'K', 'P', 'R', 'T', 'V', 'X', 'Z', 'AH') as $aCol){
 			$this->XlActiveSheet->getStyle($aCol.'2:'.$aCol.$finalrowindex)
 			->getNumberFormat()
 			->setFormatCode(\PHPExcel_Style_NumberFormat::FORMAT_DATE_DDMMYYYY);
+		}
+			//Set time formats
+		foreach (array('AG') as $aCol){
+			$this->XlActiveSheet
+				->getStyle($aCol.'2:'.$aCol.$finalrowindex)
+				->getNumberFormat()->setFormatCode('[HH]:MM')
+			;
 		}
 	}
 	
