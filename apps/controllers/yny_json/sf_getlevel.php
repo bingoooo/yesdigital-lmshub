f3304ab1d1719d8017a05e1afaa1ec6b4b9eca96
Benjamin Dant <benjamindant@gmail.com>
2016-09-14 01:20:51 +0200

    json response TEST 2

--- apps/controllers/yny_json/sf_getlevel.php
+++ apps/controllers/yny_json/sf_getlevel.php
@@ -42,6 +42,6 @@ class Sf_Getlevel extends Yny_Json {
 			$this->_view->json[$uid] = $users;
 		}*/
 		$json = '{"user_id":"01234", "firstname":"Benjamin", "lastname":"Dant", "acquired_level":"A1.1", "recommended_level":"B1.1"}'; 
-		return $json;
+		$this->_view->json = $json;
 	}
 }
