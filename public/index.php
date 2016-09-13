0a3ab4622c2e998c7ace9571faa2cfe562ba455f
Benjamin Dant <benjamindant@gmail.com>
2016-09-13 18:11:51 +0200

    retest

--- public/index.php
+++ public/index.php
@@ -1,1 +1,10 @@
-<<?php echo 'Hello World' ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>LMS-HUB</title>
+  </head>
+  <body>
+    <?php echo 'Hello World' ?>
+  </body>
+</html>
