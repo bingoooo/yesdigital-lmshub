ec3e09949b015afdbc625d1a0e18ca7898b79994
Benjamin Dant <benjamindant@gmail.com>
2016-09-14 22:56:33 +0200

    using config vars

--- apps/controllers/yny_json/sf_postgres.php
+++ apps/controllers/yny_json/sf_postgres.php
@@ -18,22 +18,23 @@ class Sf_Postgres extends Yny_Json {
 
 	function main(){
         $towns = '{"ville":"San Francisco"}';
-        $dbname = 'fhsc23783mu7c';
-        $host = 'eec2-54-228-247-206.eu-west-1.compute.amazonaws.com';
-        $dbuser = 'rnerypprnrtsjx';
-        $dbpass = 'K4SnQkbdazACuf2dNuY3O_9dwY';
+        $dbname = getenv('DATABASE');
+        $host = getenv('HOST');
+        $dbuser = getenv('USER');
+        $dbpass = getenv('PASSWORD');
         $dsn = "pgsql:host='eec2-54-228-247-206.eu-west-1.compute.amazonaws.com';dbname='dfhsc23783mu7c';user='rnerypprnrtsjx';port='5432';password='K4SnQkbdazACuf2dNuY3O_9dwY'";
         $dbopts = parse_url(getenv('DATABASE_URL'));
 		//$db = new PDO($dsn);
 		$query = 'SELECT * FROM villes';
         //$towns = $db->query($query);
 		$json2 = '{"test":"test", "message":"message"}';
-		echo $json2;
+		echo $json2.'<br>';
         foreach ($dbopts as $key => $value){
-            echo 'Key : '.$key.' : '.$value;
+            echo 'Key : '.$key.' : '.$value.'<br>';
         }
-        echo $dbopts;
-        echo $towns;
+        echo $dbname.'<br>';
+        echo $host.'<br>';
+        echo $towns.'<br>';
 	}
 
 	function checkRestrictedHosts(){
