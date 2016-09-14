a69ce9981477d82dc1a081e84213f50f31b860ec
Benjamin Dant <benjamindant@gmail.com>
2016-09-14 22:35:28 +0200

    Changed dn connection synthax TEST 03

--- apps/controllers/yny_json/sf_postgres.php
+++ apps/controllers/yny_json/sf_postgres.php
@@ -17,12 +17,13 @@ class Sf_Postgres extends Yny_Json {
 	}
 
 	function main(){
+        $towns = '{"ville":"San Francisco"}';
         $dbname = 'fhsc23783mu7c';
         $host = 'eec2-54-228-247-206.eu-west-1.compute.amazonaws.com';
         $dbuser = 'rnerypprnrtsjx';
         $dbpass = 'K4SnQkbdazACuf2dNuY3O_9dwY';
         $dsn = "pgsql:host='eec2-54-228-247-206.eu-west-1.compute.amazonaws.com';dbname='dfhsc23783mu7c';user='rnerypprnrtsjx';port='5432';password='K4SnQkbdazACuf2dNuY3O_9dwY'";
-		//$db = new PDO($dsn);
+		$db = new PDO($dsn);
 		$query = 'SELECT * FROM villes';
         //$towns = $db->query($query);
 		$json2 = '{"test":"test", "message":"message"}';
