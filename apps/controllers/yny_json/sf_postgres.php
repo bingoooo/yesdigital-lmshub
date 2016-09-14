057da812087eac8a3561119f298dc4b044f3aa24
Benjamin Dant <benjamindant@gmail.com>
2016-09-14 20:59:45 +0200

    added postgres test page

---
+++ apps/controllers/yny_json/sf_postgres.php
@@ -0,0 +1,27 @@
+<?php
+namespace FragTale\Controller\Yny_Json;
+use FragTale\Controller\Yny_Json;
+use \PDO;
+use \PDOException;
+
+class Sf_Postgres extends Yny_Json {
+	protected $dbinstance;
+
+	function initialize(){
+		parent::initialize();
+		$this->_view->setCurrentScript(TPL_ROOT.'/views/sf_json.phtml');
+	}
+
+	function doPostBack(){
+		//Nothing to code. Preventing parent behavior.
+	}
+
+	function main(){
+		$json2 = '{"test":"test", "message":"message"}';
+		else echo $json2;
+	}
+
+	function checkRestrictedHosts(){
+		return '*';
+	}
+}
