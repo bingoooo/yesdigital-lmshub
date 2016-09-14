c6e463fd402165dcf4663f99df689d1b83b5dc1c
Benjamin Dant <benjamindant@gmail.com>
2016-09-14 23:28:44 +0200

    trying using herrera pdo TEST 01

--- apps/controllers/yny_json/sf_postgres.php
+++ apps/controllers/yny_json/sf_postgres.php
@@ -3,6 +3,8 @@ namespace FragTale\Controller\Yny_Json;
 use FragTale\Controller\Yny_Json;
 use \PDO;
 use \PDOException;
+use Herrera\Pdo\PdoServiceProvider;
+use Silex\Application;
 
 class Sf_Postgres extends Yny_Json {
 	protected $dbinstance;
@@ -25,9 +27,20 @@ class Sf_Postgres extends Yny_Json {
         $port = getenv('PORT');
         $dsn = 'pgsql:dbname='.$dbname.';host='.$host.';port='.$port;
         $dbopts = parse_url(getenv('DATABASE_URL'));
-		$db = new PDO($dsn, $dbuser, $dbpass);
+		//$db = new PDO($dsn, $dbuser, $dbpass);
+
+        $app = new Application();
+        $app->register(
+            new PdoServiceProvider(),
+            array(
+                'pdo.dsn' => $dsn,
+                'pdo.username' => $dbuser,
+                'pdo.password' => $dbpass
+            )
+        );
+        $pdo = $app['pdo'];
 		$query = 'SELECT * FROM villes';
-        //$towns = $db->query($query);
+        $towns = $pdo->query($query);
 		$json = '{"test":"test", "message":"message"}';
 		echo $json;
         foreach ($dbopts as $key => $value){
