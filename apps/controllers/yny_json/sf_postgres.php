c68b5148d0029e663340dc1f9bd9431c469176d5
Benjamin Dant <benjamindant@gmail.com>
2016-09-16 01:34:51 +0200

    database seems to connect TEST 4

--- apps/controllers/yny_json/sf_postgres.php
+++ apps/controllers/yny_json/sf_postgres.php
@@ -34,7 +34,7 @@ class Sf_Postgres extends Yny_Json {
             $query->execute();
             $villes = $query->fetchAll();
 			$this->_view->json = $villes;
-			echo $villes;
+			print_r($villes);
         } else {
             echo 'no database connection ?';
         }
