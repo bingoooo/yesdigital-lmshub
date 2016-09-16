b21f9b6111c1549206e10bb94cc885a1e39f1a81
Benjamin Dant <benjamindant@gmail.com>
2016-09-16 02:23:08 +0200

    Sending JSON TEST 6

--- apps/controllers/yny_json/sf_postgres.php
+++ apps/controllers/yny_json/sf_postgres.php
@@ -33,9 +33,9 @@ class Sf_Postgres extends Yny_Json {
     		$query = $db->prepare("SELECT * FROM temps;");
             $query->execute();
             $result = $query->fetchAll();
-			$villes['towns'] = json_encode($result);
-			$this->_view->json = $villes;
-			echo $villes;
+			$villes['towns'] = $result;
+			$this->_view->json = json_encode($villes);
+			echo $this->_view->json;
         } else {
             echo 'no database connection ?';
         }
