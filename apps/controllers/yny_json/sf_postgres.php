3843932e2fcad1e040472afd72705df09d7f8010
Benjamin Dant <benjamindant@gmail.com>
2016-09-20 16:29:03 +0200

    Prod TEST 04

--- apps/controllers/yny_json/sf_postgres.php
+++ apps/controllers/yny_json/sf_postgres.php
@@ -70,11 +70,10 @@ class Sf_Postgres extends Yny_Json {
 	}
 
 	function checkRestrictedHosts(){
-		foreach ($forcedAllowedIP as $key => $value) {
-			if($value == $_SERVER['HTTP_X_FORWARDED_FOR']){
-				return '*';
-			}
+		if($_SERVER['HTTP_X_FORWARDED_FOR'] == '85.222.130.8'){
+			return '*';
+		} else {
+			$this->exitOnError(403, 'Forbidden for '.$_SERVER['HTTP_X_FORWARDED_FOR']);
 		}
-		$this->exitOnError(403, 'Forbidden for '.$_SERVER['HTTP_X_FORWARDED_FOR']);
 	}
 }
